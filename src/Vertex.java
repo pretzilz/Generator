fe6891253a1615eddc7637cccde2e0b41dc13769
Kurt Kaufman <kurt.m.kaufman@gmail.com>
2017-02-16 23:05:17 -0600

    Added vertex.

---
+++ src/Vertex.java
@@ -0,0 +1,17 @@
+package PolygonGenerator;
+public class Vertex {
+    double xCoordinate;
+    double yCoordinate;
+
+    public Vertex(double x, double y) {
+        this.xCoordinate = x;
+        this.yCoordinate = y;
+    }
+
+    public void setCoordinates(double x, double y) {
+        this.xCoordinate = x;
+        this.yCoordinate = y;
+    }
+
+    //TODO get coordinates
+}
